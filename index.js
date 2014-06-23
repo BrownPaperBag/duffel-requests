ba37f53d77864bdba27b638d54e5ef48d9e20168
Michael Robinson <mike@pagesofinterest.net>
2014-06-24 10:53:53 +1200

    Added request model getter

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   catchall: require('./lib/catchall'),
+  Request: function() {
+    return require('./lib/models/Request').model();
+  }
 };
